81ddf792a02f26cf651e25d4835d6812606c68bf
Jk <johnrambox2@yahoo.com>
2025-03-13 18:46:03 +0200

    Lesson 1 - binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CompBindComponent } from "./ang-templ/comp-bind/comp-bind.component";
+
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CompBindComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
